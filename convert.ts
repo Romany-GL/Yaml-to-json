4b7988ceb0e388b13d66bde5ca4b04296d93cbde
Romany-GL <romany.sefen@globallogic.com>
2024-04-03 00:52:17 +0100

    setup Github first workflow

--- convert.ts
+++ convert.ts
@@ -8,4 +8,4 @@ const outputFile = './output.json';
 const yamlFile = fs.readFileSync(inputFile, 'utf8');
 const data = yaml.load(yamlFile);
 fs.writeFileSync(outputFile, JSON.stringify(data, null, 2));
-console.log(data);
+console.log('data:;>>', data);
