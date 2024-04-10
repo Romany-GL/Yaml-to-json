676db09b8c470455b6be91604a7592997e282aaf
Romany-GL <romany.sefen@globallogic.com>
2024-04-10 23:25:10 +0100

    add env vars wf

--- convert.ts
+++ convert.ts
@@ -5,6 +5,16 @@ const fs = require('fs');
 const inputFile = './input.yaml';
 const outputFile = './output.json';
 
+const test = () => {
+  if (1 !== 1) {
+    return 1;
+  }
+  if (2 === 2 && 3 === 3) {
+    return 2;
+  }
+  console.log('test');
+};
+
 const yamlFile = fs.readFileSync(inputFile, 'utf8');
 const data = yaml.load(yamlFile);
 fs.writeFileSync(outputFile, JSON.stringify(data, null, 2));
