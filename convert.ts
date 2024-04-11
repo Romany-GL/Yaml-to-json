604f4cc3acd398a9795e8130e0561c21e26718dc
Romany-GL <romany.sefen@globallogic.com>
2024-04-11 13:09:10 +0100

    define new custom vars

--- convert.ts
+++ convert.ts
@@ -5,17 +5,7 @@ const fs = require('fs');
 const inputFile = './input.yaml';
 const outputFile = './output.json';
 
-const test = () => {
-  if (1 !== 1) {
-    return 1;
-  }
-  if (2 === 2 && 3 === 3) {
-    return 2;
-  }
-  console.log('test');
-};
-
 const yamlFile = fs.readFileSync(inputFile, 'utf8');
 const data = yaml.load(yamlFile);
 fs.writeFileSync(outputFile, JSON.stringify(data, null, 2));
-console.log('data:;>>', data);
+console.log('data:;>>', JSON.stringify(data, null, 2));
